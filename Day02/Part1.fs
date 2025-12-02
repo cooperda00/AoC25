b86390f4e106ff6723335f7fdd9c2e92798db189
Daniel Cooper <daniel.cooper@artlist.io>
2025-12-02 10:04:31 +0000

    feat : finished with int overload

--- Day02/Part1.fs
+++ Day02/Part1.fs
@@ -2,10 +2,15 @@ module Day02.Part1
 
 open System.IO
 
-// Iteratate over that list. If the number is odd, ignore
-// If the number is even, split it in two
-// If the first half and second half are equal - add to output sequence
-// Sum numbers in the output sequence
+type EvenOrOdd =
+    | Even
+    | Odd
+
+let getNumberOfDigits num : int = num.ToString().Length
+
+let getEvenOrOdd (numberOfDigits: int) : EvenOrOdd =
+    if numberOfDigits % 2 = 0 then Even else Odd
+
 
 let run () =
     let textContent = File.ReadAllText "./Day02/input.txt"
@@ -18,7 +23,23 @@ let run () =
             let startOfSequence = int splitOnDash.[0]
             let endOfSequence = int splitOnDash.[1]
             let fullSequence = [| startOfSequence..endOfSequence |] |> Seq.ofArray
-            fullSequence)
+
+            let filteredDoubles =
+                fullSequence
+                |> Seq.filter (fun num ->
+                    let numberOfDigits = getNumberOfDigits num
+
+                    match getEvenOrOdd numberOfDigits with
+                    | Odd -> false
+                    | Even ->
+                        let parts = num.ToString() |> Seq.splitInto 2 |> Seq.toArray
+                        let first, second = parts.[0], parts.[1]
+                        if first = second then true else false)
+
+            filteredDoubles)
+        |> Seq.concat
+        |> Seq.sum
+
 
     printfn "Day 2:1 Solution"
     printfn "%A" withSequence
