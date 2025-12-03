94277ed0ca4de210cbcd815a3c5e209b1f7979ba
Daniel Cooper <daniel.cooper@artlist.io>
2025-12-03 16:38:59 +0000

    refactor with unfold

--- Day03/Part2.fs
+++ Day03/Part2.fs
@@ -11,35 +11,18 @@ let findLargestWithinRange (startIndex: int) (endIndex: int) (arr: char array) =
 
 let calculateNumberFromLine (input: char array) =
     let finalIndex = input.Length - 1
-    let first, firstIndex = input |> findLargestWithinRange 0 (finalIndex - 11)
 
-    let second, secondIndex =
-        input |> findLargestWithinRange firstIndex (finalIndex - 10)
+    (0, 12)
+    |> List.unfold (fun (position, remaining) ->
+        if remaining = 0 then
+            None
+        else
+            let digit, nextPosition =
+                input |> findLargestWithinRange position (finalIndex - remaining + 1)
 
-    let third, thirdIndex = input |> findLargestWithinRange secondIndex (finalIndex - 9)
-
-    let fourth, fourthIndex =
-        input |> findLargestWithinRange thirdIndex (finalIndex - 8)
-
-    let fifth, fifthIndex = input |> findLargestWithinRange fourthIndex (finalIndex - 7)
-
-    let sixth, sixthIndex = input |> findLargestWithinRange fifthIndex (finalIndex - 6)
-
-    let seventh, seventhIndex =
-        input |> findLargestWithinRange sixthIndex (finalIndex - 5)
-
-    let eighth, eighthIndex =
-        input |> findLargestWithinRange seventhIndex (finalIndex - 4)
-
-    let ninth, ninthIndex = input |> findLargestWithinRange eighthIndex (finalIndex - 3)
-
-    let tenth, tenthIndex = input |> findLargestWithinRange ninthIndex (finalIndex - 2)
-
-    let eleventh, eleventhIndex =
-        input |> findLargestWithinRange tenthIndex (finalIndex - 1)
-
-    let twelfth, _ = input |> findLargestWithinRange eleventhIndex finalIndex
-    $"{first}{second}{third}{fourth}{fifth}{sixth}{seventh}{eighth}{ninth}{tenth}{eleventh}{twelfth}"
+            Some(digit, (nextPosition, remaining - 1)))
+    |> List.map string
+    |> String.concat ""
 
 
 let run () =
