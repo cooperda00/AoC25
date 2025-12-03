8ac7cbd8ae036adc8995af198293bd512399e377
Daniel Cooper <daniel.cooper@artlist.io>
2025-12-03 10:16:00 +0000

    feat : day 3 part 1

--- Day03/Part1.fs
+++ Day03/Part1.fs
@@ -2,14 +2,21 @@ module Day03.Part1
 
 open System.IO
 
-
-
-
-
 let run () =
-    let lines = File.ReadAllLines "./Day03/input.txt"
-
+    let res =
+        File.ReadAllLines "./Day03/input.txt"
+        |> Array.map (fun line ->
+            let charsArray = Seq.toArray line
+            let allButLast = Array.take (Array.length charsArray - 1) charsArray // we need at least an element at the end
+            let largest = allButLast |> Array.sortDescending |> Array.head
+            let largestIndex = Array.findIndex (fun num -> num = largest) charsArray
+            let restChars = charsArray[largestIndex + 1 ..]
+            let secondLargest = restChars |> Array.sortDescending |> Array.head
+            let digits = $"{largest}{secondLargest}"
+            let number = int digits
+            number)
+        |> Array.sum
 
 
     printfn "Day 3:1 Solution"
-// printfn "%A" withSequence
+    printfn "%A" res
