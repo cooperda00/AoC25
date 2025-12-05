ac4ff0d68c9216b208248e5e158cefd3edd1a13f
Daniel Cooper <daniel.cooper@artlist.io>
2025-12-05 09:55:50 +0000

    feat : altered algo to support very large numbers

--- Day05/Part1.fs
+++ Day05/Part1.fs
@@ -9,24 +9,32 @@ let getRangesAndIds lines =
     let filteredIds = ids |> Array.filter (fun id -> id <> "")
     ranges, filteredIds
 
-let convertRangesIntoSet (ranges: string array) =
-    ranges
-    |> Array.collect (fun range ->
-        let startAndEnd = range.Split "-"
-        let startId = int startAndEnd.[0]
-        let endId = int startAndEnd.[1]
-        [| startId..endId |])
-    |> Array.map string
-    |> Set.ofArray
+type Range = { Start: int64; End: int64 }
 
-let getNumberOfFreshIngredients (lookupSet: Set<string>) (ids: string array) : int =
-    ids |> Array.filter (fun id -> lookupSet.Contains id) |> Array.length
+let parseRange (range: string) : Range =
+    let parts = range.Split "-"
+
+    { Start = int64 parts.[0]
+      End = int64 parts.[1] }
+
+let convertRangesIntoStartAndEndRecord (ranges: string array) = ranges |> Array.map parseRange
+
+let isInRange (ranges: Range array) (id: int64) : bool =
+    ranges |> Array.exists (fun r -> id >= r.Start && id <= r.End)
+
+let getNumberOfFreshIngredients (ranges: Range array) (ids: string array) : int =
+    ids
+    |> Array.filter (fun id ->
+        let idAsInt = int64 id
+        isInRange ranges idAsInt)
+    |> Array.length
 
 let run () =
     let lines = File.ReadAllLines "./Day05/input.txt"
     let ranges, ids = getRangesAndIds lines
-    let lookupSet = convertRangesIntoSet ranges
-    let numberOfFreshIngredients = getNumberOfFreshIngredients lookupSet ids
+
+    let numberOfFreshIngredients =
+        getNumberOfFreshIngredients (convertRangesIntoStartAndEndRecord ranges) ids
 
 
     printfn "Day 5:1 Solution"
